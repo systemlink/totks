ddbfc451ec42d75c110987d329afc1d912c28600
aykkohara <a-kohara@sys-link.jp>
2018-09-29 15:58:44 +0900

    インデント修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,15 +9,15 @@ const IndexPage = () => (
       <h2>クラフト</h2>
       <section>
         <h3>マイコンの研究成果を追加しました</h3>
-  　　　　　<p>マイコンの研究成果を追加しました。(体験的マイコン学習) 今回はIchigoJamを試してみました。Ichig […]</p>
+        <p>マイコンの研究成果を追加しました。(体験的マイコン学習) 今回はIchigoJamを試してみました。Ichig […]</p>
       </section>
       <section>
         <h3>マイコンの研究成果を追加しました</h3>
-  　　　　　<p>マイコンの研究成果を追加しました。(体験的マイコン学習) 今回はmilkcocoaというBaasとWindow […]</p>
+        <p>マイコンの研究成果を追加しました。(体験的マイコン学習) 今回はmilkcocoaというBaasとWindow […]</p>
       </section>
       <section>
         <h3>マイコンの研究成果を追加しました</h3>
-  　　　　　<p>マイコンの研究成果を追加しました。(体験的マイコン学習) 今回はGR-CITRUSというマイコンを、Chrom […]</p>
+        <p>マイコンの研究成果を追加しました。(体験的マイコン学習) 今回はGR-CITRUSというマイコンを、Chrom […]</p>
       </section>
     </article>
   </Layout>
